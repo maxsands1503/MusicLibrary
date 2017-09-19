fd7b3180c15f817e7cdce26b8289211dd0fc26a6
Maxwell Sands <maxsands1503@icloud.com>
2017-09-19 10:33:25 -0600

    added song controller

--- MusicLibrary/Controllers/ArtistController.cs
+++ MusicLibrary/Controllers/ArtistController.cs
@@ -1,10 +1,76 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MusicLibrary.Models;
+using System.Linq;
+
 namespace MusicLibrary.Controllers
 {
-    public class ArtistController
-    {
-        public ArtistController()
-        {
-        }
-    }
+	[Route("api/[controller]")]
+	public class ArtistController : Controller
+	{
+		private readonly MusicContext _context;
+
+		public ArtistController(MusicContext context)
+		{
+			_context = context;
+
+			if (_context.ArtistModel.Count() == 0)
+			{
+				_context.ArtistModel.Add(new ArtistModel
+				{					
+					ArtistId = 1,
+					Name = "Black Moth Super Rainbow",
+					Year = 2012,
+					ImageUrl = "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjp1LmPzbHWAhWrw1QKHeLwBhAQjRwIBw&url=https%3A%2F%2Fwww.bbc.co.uk%2Fmusic%2Fartists%2F44e14e88-b17a-44d8-9e94-f3afa977239b&psig=AFQjCNHk7UwIljEqXQU79XdPvLSJFG1R9Q&ust=1505922616802360"
+				});
+				_context.SaveChanges();
+			}
+		}
+
+		[HttpPost]
+		public IActionResult CreateArtist([FromBody] ArtistModel artist)
+		{
+			if (artist == null)
+			{
+				return BadRequest();
+			}
+
+			_context.ArtistModel.Add(artist);
+			_context.SaveChanges();
+
+			return CreatedAtRoute("GetById", new { id = artist.ArtistId }, artist);
+
+		}
+
+		[HttpGet]
+		public IEnumerable<ArtistModel> GetAll()
+		{
+			return _context.ArtistModel.ToList();
+		}
+
+		[HttpGet("{ArtistId}", Name = "GetArtistById")]
+		public IActionResult GetById(int id)
+		{
+			var artist = _context.ArtistModel.FirstOrDefault(art => art.ArtistId == id);
+
+			if (artist == null)
+			{
+				return NotFound();
+			}
+			return new ObjectResult(artist);
+		}
+
+        [HttpGet("{ArtistId}", Name = "GetAlbumbsByArtistId")]
+		public IActionResult GetAlbumByArtistId(int id)
+		{
+			var albums = _context.AlbumModel.Where(album => album.ArtistId == id);
+
+			if (albums == null)
+			{
+				return NotFound();
+			}
+			return new ObjectResult(albums);
+		}
+	}
 }
+
---
+++ MusicLibrary/Controllers/SongController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MusicLibrary.Models;
+using System.Linq;
+
+namespace MusicLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    public class SongController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public SongController(MusicContext context)
+        {
+            _context = context;
+
+            if (_context.SongModel.Count() == 0)
+            {
+                _context.SongModel.Add(new SongModel
+                {
+                    SongId = 1,
+                    AlbumId = 1,
+                    Name = "Windshield Smasher",
+
+                });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateSong([FromBody] SongModel song)
+        {
+            if (song == null)
+            {
+                return BadRequest();
+            }
+
+            _context.SongModel.Add(song);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = song.AlbumId }, song);
+
+        }
+        [HttpGet]
+		public IEnumerable<SongModel> GetAll()
+		{
+			return _context.SongModel.ToList();
+		}
+
+		[HttpGet("{AlbumId}", Name = "GetSongsById")]
+		public IActionResult GetById(int id)
+		{
+			var songs = _context.SongModel.Where(song => song.AlbumId == id);
+
+			if (songs == null)
+			{
+				return NotFound();
+			}
+			return new ObjectResult(songs);
+		}
+
+	}
+}
